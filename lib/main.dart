1d5feed991da266285dc840452a6b25f395af1b6
Guada <125298307+GuadaGT@users.noreply.github.com>
2024-07-02 22:15:10 +0200

    inicio

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gastos/graph_widget.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(MainApp());
 
@@ -22,8 +24,98 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Widget _bottomAction(IconData icon) {
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(icon),
+      ),
+      onTap: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8.0,
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _bottomAction(FontAwesomeIcons.history),
+            _bottomAction(FontAwesomeIcons.chartPie),
+            SizedBox(width: 48.0),
+            _bottomAction(FontAwesomeIcons.wallet),
+            _bottomAction(Icons.settings),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+        shape: CircleBorder(),
+        backgroundColor: Colors.pink,
+        elevation: 6.0,
+      ),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return SafeArea(
+      child: Column(
+        children: <Widget>[_selector(), _expenses(), _graph(), _list()],
+      ),
+    );
+  }
+
+  Widget _selector() => Container();
+
+  Widget _expenses() {
+    return Column(
+      children: <Widget>[
+        Text(
+          "\$2361.41",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 16.0, color: Colors.black),
+        ),
+        Text(
+          "Total expenses",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0,
+              color: Color.fromARGB(255, 228, 86, 173)),
+        ),
+      ],
+    );
+  }
+
+  Widget _graph() {
+    return Container(height: 250.0, child: GraphWidget());
+  }
+
+  Widget _item(IconData icon, String name, int percent, double value) {
+    return ListTile(
+      leading: Icon(icon, size: 32.0),
+      title: Text(
+        name,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+      ),
+      subtitle: Text("$percent% of expenses"),
+      trailing: Text("\$$value"),
+    );
+  }
+
+  Widget _list() {
+    return Expanded(
+      child: ListView(
+        children: <Widget>[
+          _item(FontAwesomeIcons.shoppingCart, "Shopping", 14, 145.12)
+        ],
+      ),
+    );
   }
 }
